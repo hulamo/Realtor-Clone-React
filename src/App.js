5674e31f79a7480c85c7d00cdd260da1150c5ddc
hulamo <hugoladislao@gmail.com>
2022-09-26 19:31:25 -0600

    Creados Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+
 
 function App() {
   return (
-    <h1>
-    Hello world!
-  </h1>
+ <Router>
+  <Routes>
+<Route path="/" element={<Home/>}></Route>
+<Route path="/signup" element={<SignUp/>}></Route>
+<Route path="/sign-in" element={<SignIn/>}></Route>
+<Route path="/profile" element={<Profile/>}></Route>
+<Route path="/offers" element={<Offers/>}></Route>
+<Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+  </Routes>
+ </Router>
   );
 }
 
